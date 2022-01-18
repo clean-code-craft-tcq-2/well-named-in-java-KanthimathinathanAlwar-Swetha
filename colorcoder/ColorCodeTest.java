62f4b6c29275b6622ad34b757c4d1767289b6933
KanthimathinathanAlwar-Swetha <k.a.swetha850@gmail.com>
2022-01-18 13:29:21 +0530

    Reference Manual Commit

--- colorcoder/ColorCodeTest.java
+++ colorcoder/ColorCodeTest.java
@@ -8,5 +8,20 @@ public class ColorCodeTest {
 
     ColorPairTest.testPairToNumber(MajorColor.BLACK, MinorColor.ORANGE, 12);
     ColorPairTest.testPairToNumber(MajorColor.VIOLET, MinorColor.SLATE, 25);
+
+    System.out.println(colorCodeManual());
   }
+
+
+  static String colorCodeManual() {
+    System.out.println("\n");
+    System.out.println("-----25 Pair Color Code Manual-----");
+    StringBuilder colorCodeManual = new StringBuilder();
+
+    for (int i = 1; i <= 25; i++) {
+      colorCodeManual.append(i + " " + ColorCode.getColorFromPairNumber(i).toString() + "\n");
+    }
+    return colorCodeManual.toString();
+  }
+
 }
